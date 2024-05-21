a663d4d3044550a69222b469a8962ed1bd93a2d3
yaolunwei <995019397@qq.com>
2024-05-21 22:02:44 +0800

    发布v0.0.8版本

--- arch/src/main/java/com/bigoat/android/arch/datasource/LoggingInterceptor.java
+++ arch/src/main/java/com/bigoat/android/arch/datasource/LoggingInterceptor.java
@@ -1,49 +1,84 @@
 package com.bigoat.android.arch.datasource;
 
-import okhttp3.Interceptor;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
-import okio.Buffer;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okio.Buffer;
 
 public class LoggingInterceptor implements Interceptor {
+
+    private static String TAG = "HTTP";
+
+    public LoggingInterceptor() {
+    }
+
+    public LoggingInterceptor(String tag) {
+        TAG = tag;
+    }
 
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
 
         // 打印请求 URL 和请求方法
-        System.out.println("Sending request: " + request.method() + " " + request.url());
+        Log.d(TAG, "Sending request: " + request.method() + " " + request.url());
 
         // 打印请求头
-        System.out.println("Request headers: " + request.headers());
+        Log.d(TAG, "Request headers: " + request.headers());
 
         // 打印请求体
         RequestBody requestBody = request.body();
         if (requestBody != null) {
             Buffer buffer = new Buffer();
             requestBody.writeTo(buffer);
-            Charset charset = Charset.forName("UTF-8");
-            System.out.println("Request body: " + buffer.readString(charset));
+            Charset charset = StandardCharsets.UTF_8;
+            String requestBodyString = buffer.readString(charset);
+
+            try {
+                JSONObject jsonObject = new JSONObject(requestBodyString);
+                String requestBodyPrettyPrinted = jsonObject.toString(4); // 4 is the number of spaces to indent
+                Log.d(TAG, "Request body:\n" + requestBodyPrettyPrinted);
+            } catch (JSONException e) {
+                Log.e(TAG, "Failed to parse request body as JSON", e);
+            }
         }
 
         // 执行请求
         Response response = chain.proceed(request);
 
         // 打印响应信息
-        System.out.println("Received response for " + response.request().url() + ": " + response.code());
+        Log.d(TAG, "Received response for " + response.request().url() + ": " + response.code());
 
         // 打印响应体
         ResponseBody responseBody = response.body();
         if (responseBody != null) {
+            // 使用 ResponseBody.string() 方法读取响应体并打印
             String responseBodyString = responseBody.string();
-            System.out.println("Response body: " + responseBodyString);
-            // 由于调用了 responseBody.string() 方法，后续代码再次读取响应体时会抛出异常
-            // 如果需要在日志中记录响应体，应该使用 ResponseBody.charStream() 或者 ResponseBody.byteStream() 来处理响应体
+
+            try {
+                JSONObject jsonObject = new JSONObject(responseBodyString);
+                String responseBodyPrettyPrinted = jsonObject.toString(4); // 4 is the number of spaces to indent
+                Log.d(TAG, "Response body:\n" + responseBodyPrettyPrinted);
+            } catch (JSONException e) {
+                Log.e(TAG, "Failed to parse response body as JSON", e);
+            }
+
+            // 将读取后的响应体重新设置到 Response 中，因为 ResponseBody.string() 方法只能调用一次
+            responseBody = ResponseBody.create(responseBody.contentType(), responseBodyString);
+
+            // 将修改后的 Response 返回，避免抛出异常
+            return response.newBuilder().body(responseBody).build();
         }
 
         return response;
